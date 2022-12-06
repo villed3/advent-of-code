fa6a17d1130852792c7278fa8d66d137c0257d54
villed3 <villed3@outlook.com>
2022-12-06 19:44:05 +0100

    Day 5 complete

---
+++ 2022/day5/part1.py
@@ -0,0 +1,34 @@
+import os
+import queue
+# list of input stacks, as strings, in file called input.py
+import input
+
+dir = os.path.dirname(__file__)
+f = open(dir + '/input.XSCORE.txt', 'r')
+
+initial_stacks = input.STACKS
+
+stacks = []
+
+for i in range(len(initial_stacks)):
+    stack = queue.LifoQueue()
+    for c in initial_stacks[i]:
+        stack.put(c)
+    stacks.append(stack)
+
+while True:
+    line = f.readline()
+    if line == '':
+        break
+
+    [count, s1, s2] = map(int, line.replace('move ', '').replace('from ', '').replace('to ', '').split())
+
+    for i in range(0, count):
+        stacks[s2 - 1].put(stacks[s1 - 1].get())
+
+top_items = ''
+
+for i in range(len(stacks)):
+    top_items += stacks[i].get()
+
+print(top_items)
